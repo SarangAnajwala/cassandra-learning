ef83c9c026d9bec83360bd10e61dd457381285c6
Sarang Anajwala <Sarang.Anajwala@gmail.com>
2013-01-11 14:35:55 +0530

    Correction in javadocs comment

--- src/main/java/com/blogspot/oakgreen/cassandra/cassandralearning/RowKeyDateMain.java
+++ src/main/java/com/blogspot/oakgreen/cassandra/cassandralearning/RowKeyDateMain.java
@@ -7,50 +7,52 @@ import com.blogspot.oakgreen.cassandra.cassandralearning.domain.RowKeyDate;
 
 /**
  * This program is written to test how does default ordering (by rowkey) works
- * when rowkey is of 'DateType'. The results show that one MUST NOT use DateType
- * for rowkey as the ordering gets all messed up!
+ * when rowkey is of 'DateType'. The results are positive - the sorting is done 
+ * as simple string sort.
  * 
  * Sample result:
- * [default@Learning] list RowkeyDate;
+ * [default@Learning] list rowKeyDate;
  * Using default limit of 100
  * Using default column limit of 100
  * -------------------
- * RowKey: 2013-01-13 02:23:13+0530
- * => (column=colDate1, value=2013-01-13 02:23:13+0530, timestamp=1357851193528004)
- * => (column=colDate2, value=2013-01-13 02:22:13+0530, timestamp=1357851193528007)
- * => (column=colDate3, value=2013-01-13 02:21:13+0530, timestamp=1357851193528005)
- * => (column=rkDate, value=2013-01-13 02:23:13+0530, timestamp=1357851193528006)
+ * RowKey: 2013-01-01 14:31:15+0530
+ * => (column=colDate1, value=2013-01-01 14:23:15+0530, timestamp=1357894875778007)
+ * => (column=colDate2, value=2013-01-01 14:22:15+0530, timestamp=1357894875778004)
+ * => (column=colDate3, value=2013-01-01 14:21:15+0530, timestamp=1357894875778006)
+ * => (column=rkDate, value=2013-01-01 14:31:15+0530, timestamp=1357894875778005)
  * -------------------
- * RowKey: 2013-01-29 02:22:13+0530
- * => (column=colDate1, value=2013-01-29 02:13:13+0530, timestamp=1357851133853003)
- * => (column=colDate2, value=2013-01-29 02:12:13+0530, timestamp=1357851133853001)
- * => (column=colDate3, value=2013-01-29 02:11:13+0530, timestamp=1357851133853002)
- * => (column=rkDate, value=2013-01-29 02:22:13+0530, timestamp=1357851133853000)
+ * RowKey: 2013-01-02 14:31:15+0530
+ * => (column=colDate1, value=2013-01-02 14:13:15+0530, timestamp=1357894875778003)
+ * => (column=colDate2, value=2013-01-02 14:12:15+0530, timestamp=1357894875778000)
+ * => (column=colDate3, value=2013-01-02 14:11:15+0530, timestamp=1357894875778002)
+ * => (column=rkDate, value=2013-01-02 14:31:15+0530, timestamp=1357894875778001)
  * -------------------
- * RowKey: 2013-01-01 02:21:13+0530
- * => (column=colDate1, value=2013-01-01 02:23:13+0530, timestamp=1357851073094006)
- * => (column=colDate2, value=2013-01-01 02:22:13+0530, timestamp=1357851073094004)
- * => (column=colDate3, value=2013-01-01 02:21:13+0530, timestamp=1357851073094007)
- * => (column=rkDate, value=2013-01-01 02:21:13+0530, timestamp=1357851073094005)
+ * RowKey: 2013-01-03 14:31:15+0530
+ * => (column=colDate1, value=2013-01-03 14:03:15+0530, timestamp=1357894875106002)
+ * => (column=colDate2, value=2013-01-03 14:02:15+0530, timestamp=1357894875075000)
+ * => (column=colDate3, value=2013-01-03 14:01:15+0530, timestamp=1357894875106001)
+ * => (column=rkDate, value=2013-01-03 14:31:15+0530, timestamp=1357894875106000)
  * -------------------
- * RowKey: 2013-01-30 02:22:13+0530
- * => (column=colDate1, value=2013-01-30 02:03:13+0530, timestamp=1357851133806003)
- * => (column=colDate2, value=2013-01-30 02:02:13+0530, timestamp=1357851133806001)
- * => (column=colDate3, value=2013-01-30 02:01:13+0530, timestamp=1357851133806002)
- * => (column=rkDate, value=2013-01-30 02:22:13+0530, timestamp=1357851133806000)
+ * RowKey: 2013-01-28 14:31:38+0530
+ * => (column=colDate1, value=2013-01-28 14:23:38+0530, timestamp=1357894898871007)
+ * => (column=colDate2, value=2013-01-28 14:22:38+0530, timestamp=1357894898871005)
+ * => (column=colDate3, value=2013-01-28 14:21:38+0530, timestamp=1357894898871006)
+ * => (column=rkDate, value=2013-01-28 14:31:38+0530, timestamp=1357894898871004)
  * -------------------
- * RowKey: 2013-01-02 02:21:13+0530
- * => (column=colDate1, value=2013-01-02 02:13:13+0530, timestamp=1357851073094002)
- * => (column=colDate2, value=2013-01-02 02:12:13+0530, timestamp=1357851073094000)
- * => (column=colDate3, value=2013-01-02 02:11:13+0530, timestamp=1357851073094003)
- * => (column=rkDate, value=2013-01-02 02:21:13+0530, timestamp=1357851073094001)
+ * RowKey: 2013-01-29 14:31:38+0530
+ * => (column=colDate1, value=2013-01-29 14:13:38+0530, timestamp=1357894898871003)
+ * => (column=colDate2, value=2013-01-29 14:12:38+0530, timestamp=1357894898871001)
+ * => (column=colDate3, value=2013-01-29 14:11:38+0530, timestamp=1357894898871002)
+ * => (column=rkDate, value=2013-01-29 14:31:38+0530, timestamp=1357894898871000)
  * -------------------
- * RowKey: 2013-01-15 02:23:13+0530
- * => (column=colDate1, value=2013-01-15 02:03:13+0530, timestamp=1357851193481000)
- * => (column=colDate2, value=2013-01-15 02:02:13+0530, timestamp=1357851193481003)
- * => (column=colDate3, value=2013-01-15 02:01:13+0530, timestamp=1357851193481001)
- * => (column=rkDate, value=2013-01-15 02:23:13+0530, timestamp=1357851193481002)
- * -------------------
+ * RowKey: 2013-01-30 14:31:38+0530
+ * => (column=colDate1, value=2013-01-30 14:03:38+0530, timestamp=1357894898825002)
+ * => (column=colDate2, value=2013-01-30 14:02:38+0530, timestamp=1357894898825000)
+ * => (column=colDate3, value=2013-01-30 14:01:38+0530, timestamp=1357894898825001)
+ * => (column=rkDate, value=2013-01-30 14:31:38+0530, timestamp=1357894898809000)
+ * 
+ * 6 Rows Returned.
+ * Elapsed time: 76 msec(s).
  * 
  * @author Sarang Anajwala (http://OakGreen.blogspot.com
  * 
@@ -71,7 +73,7 @@ public class RowKeyDateMain extends GenericMain<RowKeyDate> {
 		//Calendar tmp = cal;
 		for (int i=0; i < 3; i++) {
 			cal = Calendar.getInstance();
-			cal.set(Calendar.DAY_OF_MONTH, (3*5 - i));
+			cal.set(Calendar.DAY_OF_MONTH, (3 - i));
 			//cal.set(Calendar.MINUTE, 0);
 			rkDt = cal.getTime();
 			cal.set(Calendar.MINUTE, (10*i + 1));
